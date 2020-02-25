d47ce20f9a263a909886c98b76d1f19299902b24
Shane Moore <shane@aylien.com>
2020-02-25 10:16:17 +0000

    feat: Initial commit of gem structure

---
+++ test/taxonomy_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class TaxonomyTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Taxonomy::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
